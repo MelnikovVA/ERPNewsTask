8b1ae1b5fbb0cb5162ed9ea015fc55c6f9d03ac6
Vladimir Melnikov <vladmeln158@yandex.ru>
2019-06-24 16:08:17 +0700

    Implemented data fetching
    Need to fix HTML tags and correct the viewing on Android

--- ERPNews/ERPNews/ERPNews/MainPage.xaml.cs
+++ ERPNews/ERPNews/ERPNews/MainPage.xaml.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
 using Xamarin.Forms;
+using ERPNews.Models;
 
 namespace ERPNews
 {
@@ -12,6 +15,23 @@ namespace ERPNews
         public MainPage()
         {
             InitializeComponent();
+
+            GetNews();
+        }
+
+        private async void GetNews()
+        {
+            HttpClient client = new HttpClient();
+            String url = "https://education-erp.com/api/ClientApplication/News?schoolType=Football&cityId=4&count=10";
+            var response = await client.GetStringAsync(url);
+
+            var newsList = JsonConvert.DeserializeObject<List<News>>(response);
+            NewsListView.ItemsSource = newsList;
+        }
+
+        private void OnItemSelected(object sender, ItemTappedEventArgs e)
+        {
+
         }
     }
 }
