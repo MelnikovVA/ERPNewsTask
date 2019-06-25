3bb7c2e8915bace14440b2f47a010285f405c788
Vladimir Melnikov <vladmeln158@yandex.ru>
2019-06-25 19:50:26 +0700

    Finished HTML concatenation
    Not sure it's the way to do that, but it works, in a sense

--- ERPNews/ERPNews/ERPNews/ItemDetailsPage.xaml.cs
+++ ERPNews/ERPNews/ERPNews/ItemDetailsPage.xaml.cs
@@ -13,16 +13,44 @@ namespace ERPNews
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ItemDetailsPage : ContentPage
 	{
-		public ItemDetailsPage (News news)
+        HtmlWebViewSource htmlSource = new HtmlWebViewSource();
+
+        public ItemDetailsPage (News news)
 		{
 			InitializeComponent ();
-            var htmlSource = new HtmlWebViewSource();
+            BindingContext = news;
+            
+            ConcatHtml(news);
+            webView.Source = htmlSource;
+        }
+
+        private void ConcatHtml(News news)
+        {
+
             htmlSource.Html = news.FormattedText;
 
-            var htmlTitle = "<b>" + news.Title + "</b><br />";
-            //webView.Source = htmlSource;
+            var htmlTitle = "<big><b>" + news.Title + "</big></b><br />";
+
+            var htmlCreationDate = "<p><small>" + DateTime.Parse(news.DateTime.ToString()).ToString() + "</small></p><br />";
+
+            string htmlMainImg;
+            if (Device.RuntimePlatform == Device.UWP)
+                htmlMainImg = "<img src=\"" + news.MainImagePath + "\" style = \"width:50%; height:auto; margin: 5 auto;\"><br/>";
+            else
+                htmlMainImg = "<img src=\"" + news.MainImagePath + "\" style = \"width:99%; height:auto; margin: 5 auto;\">";
 
 
+            htmlSource.Html = htmlTitle + htmlMainImg + htmlSource.Html + htmlCreationDate;
+
+            string htmlAdditionalImg;
+            foreach (var additionalPath in news.AdditionalImagesPaths)
+            {
+                if (Device.RuntimePlatform == Device.UWP)
+                    htmlAdditionalImg = "<img src=\"" + additionalPath + "\" style = \"width:50%; height:auto; margin: 10 auto;\"><br/>";
+                else
+                    htmlAdditionalImg = "<img src=\"" + additionalPath + "\" style = \"width:99%; height:auto; margin: 5 auto;\">";
+                htmlSource.Html += htmlAdditionalImg;
+            }
         }
 	}
 }
